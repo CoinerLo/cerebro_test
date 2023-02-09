f1dd9680db86b32575dffe54b7cb5f7525663d28
CoinerLo <coiner4677@gmail.com>
2023-02-09 20:59:29 +0300

    Changes according to the task

--- task_two/src/App.tsx
+++ task_two/src/App.tsx
@@ -3,16 +3,12 @@ import { Input } from './Input'
 import './App.css'
 
 function App() {
-  const [currentNumber, setCurrentNumber] = useState<undefined | string>(undefined)
-
-  const changeHandler = useCallback(({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentNumber(target.value)
-  }, [])
+  const [currentNumber, setCurrentNumber] = useState<undefined | number>(undefined)
 
   return (
     <form className="App">
       <h1>Введите число</h1>
-      <Input currentNumber={currentNumber} changeHandler={changeHandler} className='fontLarge' />
+      <Input currentNumber={currentNumber} setCurrentNumber={setCurrentNumber} className='fontLarge' />
     </form>
   )
 }
--- task_two/src/Input.tsx
+++ task_two/src/Input.tsx
@@ -1,19 +1,32 @@
-import { FC } from "react"
+import { Dispatch, FC, SetStateAction } from "react"
 
 interface InputProps {
-  currentNumber: undefined | string
-  changeHandler: (event: React.ChangeEvent<HTMLInputElement>) => void
+  currentNumber: undefined | number
+  setCurrentNumber: Dispatch<SetStateAction<number | undefined>>
   className?: string
 }
 
-export const Input: FC<InputProps> = ({ currentNumber, changeHandler, className }) => {
+export const Input: FC<InputProps> = ({ currentNumber, setCurrentNumber, className }) => {
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const valueString = e.target.value
+    if (!valueString) {
+      setCurrentNumber(undefined)
+    } else {
+      const valueFloat = parseFloat(valueString.replace(',', '.'))
+      if (valueFloat.toString() === valueString) {
+        setCurrentNumber(valueFloat)
+        console.log(currentNumber)
+      }
+    }
+  }
+
   return (
     <input
       name="current_number"
       type="number"
       className={className ?? ''}
-      onChange={changeHandler}
-      value={currentNumber}
+      onChange={onChangeInput}
+      value={currentNumber?.toString() ?? ''}
       step="any"
     />
   )
