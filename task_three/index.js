950dc612eff082ee4873a2c73209fa6370a5538f
CoinerLo <coiner4677@gmail.com>
2023-02-09 21:00:07 +0300

    Changes according to the task

--- task_three/index.js
+++ task_three/index.js
@@ -3,8 +3,18 @@ const arrayB = [2, 4, 7, 8, 11, 14, 15, 17, 20, 24, 25, 29, 30, 35, 39, 40, 41, 
 
 const arrayC = arrayA.filter(element => !arrayB.includes(element)) // Значения есть в arrayA, но нет в arrayB
 
-console.log(arrayC)
+const objA = {}
+arrayB.forEach(b => { objA[b] = true })
+const arrayC_versionTwo = arrayA.filter(e => !objA[e])
+
+console.log(arrayC, arrayC_versionTwo)
 
 const arrayD = [...new Set(arrayA.filter(element => arrayB.includes(element)))] // Значения есть в обоих массивах, дубликаты удалить
 
-console.log(arrayD)
+// создать массив в котором содержатся все числа, которые есть в обоих массивах А и В.
+// порядок не важен, дубликаты удалить
+
+const setD = new Set([...arrayA, ...arrayB])
+const arrayD_versionTwo = Array.from(setD)
+
+console.log(arrayD, arrayD_versionTwo)
